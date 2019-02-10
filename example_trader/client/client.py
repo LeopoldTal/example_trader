6edf00d43a0b4c478c460edf1ec4c4ddabe29314
Leopold Tal G <leopold.tal.dg@gmail.com>
2019-02-11 00:08:17 +0100

    keep connection alive between reads

--- example_trader/client/client.py
+++ example_trader/client/client.py
@@ -5,13 +5,14 @@ class Client:
 	"""Price stream API client"""
 	def __init__(self, url):
 		self.url = url
+		self.session = requests.Session() # to keep connection alive
 	
 	def get_price(self):
 		"""C.get_price()
 		
 		Get the current asset price"""
 		# TODO: maintain a session to keep the connection alive
-		price_req = requests.get(self.url)
+		price_req = self.session.get(self.url)
 		if price_req.status_code == 200:
 			return int(price_req.text)
 		else:
