ce9e50425cd23ed04c969ffb856309ba92194c6f
Leopold Tal G <leopold.tal.dg@gmail.com>
2019-02-10 20:35:44 +0100

    add extremely simple trading agent

---
+++ example_trader/agent/agent.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+class Agent:
+	"""An agent, trading some asset against some currency"""
+	def __init__(self):
+		self.cash = 1000
+		self.asset = 0
+	
+	"""A.get_buy_amount(price_history, current_price) -> float
+	
+	Get amount of the asset the agent wants to buy.
+	Negative for selling, 0 for holding"""
+	def get_buy_amount(self, price_history, current_price):
+		if len(price_history) == 0:
+			return 0
+		
+		mean_price = np.mean(price_history)
+		if current_price < mean_price: # TODO: what's a bid-ask spread, precious?
+			if current_price <= self.cash:
+				return 1 # TODO: pick some sensible amount; Kelly?
+			else: # TODO: buy partial asset?
+				return 0
+		else:
+			if self.asset >= 1:
+				return -1
+			else:
+				return 0
+	
+	"""A.record(buy_amount, price)
+	
+	Record a purchase or sale of the asset at the given unit price"""
+	def record(self, buy_amount, price):
+		self.asset += buy_amount
+		self.cash -= buy_amount * price
---
+++ tests/test_agent.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from .context import example_trader
+
+def test_holds_if_knows_nothing():
+	agent = example_trader.agent.Agent()
+	buy_amount = agent.get_buy_amount(price_history=[], current_price=1.)
+	assert(buy_amount) == 0
+
+def test_no_buy_if_broke():
+	agent = example_trader.agent.Agent()
+	agent.cash = 0
+	buy_amount = agent.get_buy_amount(price_history=[1000.,1000.,1000.1,999.9], current_price=1.)
+	assert(buy_amount) == 0
+
+def test_no_shorts():
+	agent = example_trader.agent.Agent()
+	agent.asset = 0
+	buy_amount = agent.get_buy_amount(price_history=[.01,.01,0.011,0.009], current_price=1.)
+	assert(buy_amount) == 0
+
+def test_buys_at_huge_crash():
+	agent = example_trader.agent.Agent()
+	buy_amount = agent.get_buy_amount(price_history=[1000.,1000.,1000.1,999.9], current_price=1.)
+	assert(buy_amount) > 0
+
+def test_sells_at_huge_spike():
+	agent = example_trader.agent.Agent()
+	agent.asset = 42
+	buy_amount = agent.get_buy_amount(price_history=[.01,.01,0.011,0.009], current_price=1.)
+	assert(buy_amount) < 0
+
+def test_records_buy():
+	agent = example_trader.agent.Agent()
+	agent.cash = 100
+	agent.asset = 50
+	agent.record(buy_amount = 42, price = 0.1)
+	assert(agent.asset == 92)
+	assert(agent.cash == 95.8)
